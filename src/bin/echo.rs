5c504a057c02cb8c6dfac299e1506152f75580aa
Nathan Stocks <nathan.stocks@gmail.com>
2017-02-19 13:27:41 -0700

    Figured out a decent way to deal with strings and stuff.  Soooo complicated.

---
+++ src/bin/echo.rs
@@ -0,0 +1,53 @@
+/*-
+ * Copyright (c) 1989, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+fn main () {
+    let mut add_newline = true;
+    let mut args : Vec<String> = std::env::args().collect();
+    // We don't care about the program name
+    args.remove(0);
+    // The first argument could be "-n"
+    if args[0] == "-n" {
+        add_newline = false;
+        args.remove(0);
+    }
+    // Any arguments get a space in between them
+    let out_string = args.join(" ");
+    // The odd behavior of ending the last argument with "\c" means don't output a newlin
+    if out_string.ends_with("\\c") {
+        add_newline = false;
+        print!("{}", &out_string[..out_string.len()-2]);
+    } else {
+        print!("{}", out_string);
+    }
+    // Unless the user specificed "-n" as the first argument, end with a newline
+    if add_newline {
+        print!("\n");
+    }
+}
