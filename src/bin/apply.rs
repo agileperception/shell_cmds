116e2262ade8d39bd18a3f64811dd8d476f4c921
Nathan Stocks <nathan.stocks@gmail.com>
2017-03-07 23:56:02 -0700

    Made some actual progress translating the ancient heiroglyphics (C code with 1-letter variable names) into readable (and functional) code.

--- src/bin/apply.rs
+++ src/bin/apply.rs
@@ -34,7 +34,6 @@
  * SUCH DAMAGE.
  */
 
-#[macro_use(value_t)]
 extern crate clap;
 use clap::{Arg, App};
 
@@ -86,25 +85,25 @@ fn main() {
 
     let command_arguments : Vec<&str> = args.values_of("command_arguments").unwrap().collect();
 
-    // Set num_args_at_a_time based off of one of the -0123456789 arguments.
-    let mut num_args_at_a_time : u8 = 1;
-    for number_str in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].iter() {
+    // Set num_args_per_command based off of one of the -0123456789 arguments.
+    let mut num_args_per_command : i8 = -1;
+    for number_str in &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] {
         if args.is_present(number_str) {
-            if num_args_at_a_time != 99 {
+            if num_args_per_command != -1 {
                 errx("only one -# argument may be specified");
             }
-            num_args_at_a_time = u8::from_str(number_str).unwrap();
+            num_args_per_command = i8::from_str(number_str).unwrap();
         }
     }
 
-    // Presence of magic character numbers overrides -0123456789 arguments.
-    // The logic is to use the highest numerical value seen
-    for number_char in ["9", "8", "7", "6", "5", "4", "3", "2", "1"].iter() {
+    // Find the highest numerical value seen in the command string
+    let mut max_argnum_in_command : i8 = 0;
+    for number_char in &["9", "8", "7", "6", "5", "4", "3", "2", "1"] {
         let mut search_str = String::with_capacity(2);
         search_str += magic_char;
         search_str += number_char;
-        if let Some(_) = command.find(&search_str) {
-            num_args_at_a_time = u8::from_str(number_char).unwrap();
+        if command.find(&search_str).is_some() {
+            max_argnum_in_command = i8::from_str(number_char).unwrap();
             break;
         }
     }
@@ -116,13 +115,32 @@ fn main() {
         Err(_) => "/bin/sh".to_string(),
     };
 
+    let mut command_template = String::from_str("exec ").unwrap();
+    if max_argnum_in_command == 0 {
+        if num_args_per_command == -1 {
+            num_args_per_command = 1;
+        }
+        command_template.push_str(command);
+        for i in 1..(num_args_per_command + 1) {
+            command_template.push(' ');
+            command_template.push_str(magic_char);
+            command_template.push_str(&(i.to_string()));
+        }
+        // XXX
+    } else {
+        command_template.push_str(command);
+        num_args_per_command = max_argnum_in_command;
+        
+    }
+
     println!("shell is {}", shell);
-    println!("num_args_at_a_time is {}", num_args_at_a_time);
-    println!("command is {}", command);
-    println!("command_arguments are {:?}", command_arguments);
     println!("debug is {}", debug);
     println!("magic_char is {:?}", magic_char);
-
+    println!("num_args_per_command is {}", num_args_per_command);
+    println!("max_argnum_in_command is {}", max_argnum_in_command);
+    println!("command is {}", command);
+    println!("command_arguments are {:?}", command_arguments);
+    println!("command_template is {}", command_template)
 
 
 }
